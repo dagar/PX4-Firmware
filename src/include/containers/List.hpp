66b9569c6f89c357786d052bc2c6b32afc89dc60
Daniel Agar <daniel@agar.ca>
2019-02-22 13:34:23 -0500

    WIP

--- src/include/containers/List.hpp
+++ src/include/containers/List.hpp
@@ -39,7 +39,7 @@
 
 #pragma once
 
-template<class T>
+template<typename T>
 class ListNode
 {
 public:
@@ -47,16 +47,68 @@ public:
 	void setSibling(T sibling) { _sibling = sibling; }
 	const T getSibling() const { return _sibling; }
 
+	void removeSibling() { _sibling = nullptr; }
+
 protected:
 
 	T _sibling{nullptr};
 
 };
 
-template<class T>
+template<typename T>
 class List
 {
 public:
+
+	class Iterator
+	{
+	public:
+		explicit Iterator(T node): _node(node) {}
+		Iterator() = delete;
+
+		T operator*() { return _node; }
+		bool operator!=(const Iterator &it) const { return value() != it.value(); }
+
+		Iterator &operator++()
+		{
+			_node = _node->getSibling();
+			return *this;
+		}
+
+		const T &value() const { return _node; }
+
+	private:
+		T _node;
+	};
+
+	Iterator begin() { return Iterator{front()}; }
+	Iterator end() { return Iterator{back()}; }
+
+	void push_front(T newNode)
+	{
+		newNode->setSibling(front());
+		_head = newNode;
+	}
+
+//	void push_back(T newNode)
+//	{
+//		newNode->setSibling(back());
+//		_tail = newNode;
+//	}
+
+	void pop_front()
+	{
+		T &oldHead = _head;
+		_head = _head->getSibling();
+		oldHead->removeSibling();
+	}
+
+//	void pop_back()
+//	{
+//		T& oldTail = _tail;
+//		_tail = _head->getSibling();
+//		oldHead.removeSibling();
+//	}
 
 	void add(T newNode)
 	{
@@ -64,9 +116,63 @@ public:
 		_head = newNode;
 	}
 
+	bool remove(const T removeNode)
+	{
+		if (removeNode == _head) {
+			_head = nullptr;
+			return true;
+
+		} else if (removeNode == _tail) {
+			// TODO: find node pointing to tail and update
+			_tail = nullptr;
+			return true;
+
+		} else {
+			for (T node = _head; node != nullptr; node = node->getSibling()) {
+				// is sibling the node to remove?
+				if (node->getSibling() == removeNode) {
+					// replace sibling
+					auto sibling = node->getSibling();
+
+					if (sibling != nullptr) {
+						node->setSibling(sibling->getSibling());
+
+					} else {
+						node->removeSibling();
+					}
+
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
 	const T getHead() const { return _head; }
+
+	bool empty() const { return _head == nullptr; }
+
+	T front() const { return _head; }
+	T back() const { return _tail; }
+
+	// range based for loop
+	// comparison operator?
+
+	// cbegin
+	// cend
+
+//	T begin() { return _head; }
+//	T end() { return _tail; }
+//
+//	const T begin() const { return _head; }
+//	const T end() const { return _tail; }
+
+	//iterator operator++() { ++ptr; return *this; }
 
 protected:
 
 	T _head{nullptr};
+	T _tail{nullptr};
+
 };
