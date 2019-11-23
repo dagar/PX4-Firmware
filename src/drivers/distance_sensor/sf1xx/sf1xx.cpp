780f528789df3e56c97beaf232bda1ef8f62870f
Daniel Agar <daniel@agar.ca>
2019-11-22 21:51:49 -0500

    sf1xx cleanup

--- src/drivers/distance_sensor/sf1xx/sf1xx.cpp
+++ src/drivers/distance_sensor/sf1xx/sf1xx.cpp
@@ -89,18 +89,12 @@ public:
 
 	int init() override;
 
-	ssize_t read(device::file_t *filp, char *buffer, size_t buflen) override;
-	int ioctl(device::file_t *filp, int cmd, unsigned long arg) override;
-
-	/**
-	* Diagnostics - print some basic information about the driver.
-	*/
 	void print_info();
 
-protected:
+private:
+
 	int probe() override;
 
-private:
 	/**
 	* Test whether the device supported by the driver is present at a
 	* specific address.
@@ -117,21 +111,7 @@ private:
 	*       to make it more aggressive about resetting the bus in case of errors.
 	*/
 	void start();
-
-	/**
-	* Stop the automatic measurement state machine.
-	*/
 	void stop();
-
-	/**
-	* Set the min and max distance thresholds if you want the end points of the sensors
-	* range to be brought in at all, otherwise it will use the defaults SF1XX_MIN_DISTANCE
-	* and SF1XX_MAX_DISTANCE
-	*/
-	void set_minimum_distance(float min);
-	void set_maximum_distance(float max);
-	float get_minimum_distance();
-	float get_maximum_distance();
 
 	/**
 	* Perform a poll cycle; collect from the previous measurement
@@ -146,52 +126,22 @@ private:
 	int _class_instance{-1};
 	int _conversion_interval{-1};
 	int _measure_interval{0};
-	int _orb_class_instance{-1};
 
-	float _max_distance{-1.0f};
-	float _min_distance{-1.0f};
-
-	uint8_t _rotation{0};
-
-	ringbuffer::RingBuffer  *_reports{nullptr};
-
-	orb_advert_t _distance_sensor_topic{nullptr};
-
-	perf_counter_t _sample_perf{perf_alloc(PC_ELAPSED, "sf1xx_read")};
-	perf_counter_t _comms_errors{perf_alloc(PC_COUNT, "sf1xx_com_err")};
+	perf_counter_t _sample_perf{perf_alloc(PC_ELAPSED, MODULE_NAME": read")};
+	perf_counter_t _comms_errors{perf_alloc(PC_COUNT, MODULE_NAME": com err")};
 };
-
-/*
- * Driver 'main' command.
- */
-extern "C" __EXPORT int sf1xx_main(int argc, char *argv[]);
 
 SF1XX::SF1XX(uint8_t rotation, int bus, int address) :
 	I2C("SF1XX", SF1XX_DEVICE_PATH, bus, address, 400000),
 	ScheduledWorkItem(MODULE_NAME, px4::device_bus_to_wq(get_device_id())),
-	_rotation(rotation)
+	_px4_rangefinder(get_device_id(), ORB_PRIO_DEFAULT, rotation)
 {
 }
 
 SF1XX::~SF1XX()
 {
-	/* make sure we are truly inactive */
 	stop();
 
-	/* free any existing reports */
-	if (_reports != nullptr) {
-		delete _reports;
-	}
-
-	if (_distance_sensor_topic != nullptr) {
-		orb_unadvertise(_distance_sensor_topic);
-	}
-
-	if (_class_instance != -1) {
-		unregister_class_devname(RANGE_FINDER_BASE_DEVICE_PATH, _class_instance);
-	}
-
-	/* free perf counters */
 	perf_free(_sample_perf);
 	perf_free(_comms_errors);
 }
@@ -200,7 +150,7 @@ int
 SF1XX::init()
 {
 	int ret = PX4_ERROR;
-	int hw_model;
+	int32_t hw_model = 0;
 	param_get(param_find("SENS_EN_SF1XX"), &hw_model);
 
 	switch (hw_model) {
@@ -209,41 +159,41 @@ SF1XX::init()
 		return ret;
 
 	case 1:  /* SF10/a (25m 32Hz) */
-		_min_distance = 0.01f;
-		_max_distance = 25.0f;
+		_px4_rangefinder.set_min_distance(0.01f);
+		_px4_rangefinder.set_max_distance(25.0f);
 		_conversion_interval = 31250;
 		break;
 
 	case 2:  /* SF10/b (50m 32Hz) */
-		_min_distance = 0.01f;
-		_max_distance = 50.0f;
+		_px4_rangefinder.set_min_distance(0.01f);
+		_px4_rangefinder.set_max_distance(50.0f);
 		_conversion_interval = 31250;
 		break;
 
 	case 3:  /* SF10/c (100m 16Hz) */
-		_min_distance = 0.01f;
-		_max_distance = 100.0f;
+		_px4_rangefinder.set_min_distance(0.01f);
+		_px4_rangefinder.set_max_distance(100.0f);
 		_conversion_interval = 62500;
 		break;
 
 	case 4:
 		/* SF11/c (120m 20Hz) */
-		_min_distance = 0.01f;
-		_max_distance = 120.0f;
+		_px4_rangefinder.set_min_distance(0.01f);
+		_px4_rangefinder.set_max_distance(120.0f);
 		_conversion_interval = 50000;
 		break;
 
 	case 5:
 		/* SF/LW20/b (50m 48-388Hz) */
-		_min_distance = 0.001f;
-		_max_distance = 50.0f;
+		_px4_rangefinder.set_min_distance(0.001f);
+		_px4_rangefinder.set_max_distance(50.0f);
 		_conversion_interval = 20834;
 		break;
 
 	case 6:
 		/* SF/LW20/c (100m 48-388Hz) */
-		_min_distance = 0.001f;
-		_max_distance = 100.0f;
+		_px4_rangefinder.set_min_distance(0.001f);
+		_px4_rangefinder.set_max_distance(100.0f);
 		_conversion_interval = 20834;
 		break;
 
@@ -257,26 +207,7 @@ SF1XX::init()
 		return ret;
 	}
 
-	/* allocate basic report buffers */
-	_reports = new ringbuffer::RingBuffer(2, sizeof(distance_sensor_s));
-
 	set_device_address(SF1XX_BASEADDR);
-
-	if (_reports == nullptr) {
-		return ret;
-	}
-
-	_class_instance = register_class_devname(RANGE_FINDER_BASE_DEVICE_PATH);
-
-	/* get a publish handle on the range finder topic */
-	struct distance_sensor_s ds_report = {};
-
-	_distance_sensor_topic = orb_advertise_multi(ORB_ID(distance_sensor), &ds_report,
-				 &_orb_class_instance, ORB_PRIO_HIGH);
-
-	if (_distance_sensor_topic == nullptr) {
-		PX4_ERR("failed to create distance_sensor object");
-	}
 
 	// Select altitude register
 	int ret2 = measure();
@@ -297,162 +228,12 @@ SF1XX::probe()
 	return measure();
 }
 
-void
-SF1XX::set_minimum_distance(float min)
-{
-	_min_distance = min;
-}
-
-void
-SF1XX::set_maximum_distance(float max)
-{
-	_max_distance = max;
-}
-
-float
-SF1XX::get_minimum_distance()
-{
-	return _min_distance;
-}
-
-float
-SF1XX::get_maximum_distance()
-{
-	return _max_distance;
-}
-
-int
-SF1XX::ioctl(device::file_t *filp, int cmd, unsigned long arg)
-{
-	switch (cmd) {
-
-	case SENSORIOCSPOLLRATE: {
-			switch (arg) {
-
-			/* zero would be bad */
-			case 0:
-				return -EINVAL;
-
-			/* set default polling rate */
-			case SENSOR_POLLRATE_DEFAULT: {
-					/* do we need to start internal polling? */
-					bool want_start = (_measure_interval == 0);
-
-					/* set interval for next measurement to minimum legal value */
-					_measure_interval = (_conversion_interval);
-
-					/* if we need to start the poll state machine, do it */
-					if (want_start) {
-						start();
-
-					}
-
-					return OK;
-				}
-
-			/* adjust to a legal polling interval in Hz */
-			default: {
-					/* do we need to start internal polling? */
-					bool want_start = (_measure_interval == 0);
-
-					/* convert hz to tick interval via microseconds */
-					int interval = (1000000 / arg);
-
-					/* check against maximum rate */
-					if (interval < _conversion_interval) {
-						return -EINVAL;
-					}
-
-					/* update interval for next measurement */
-					_measure_interval = interval;
-
-					/* if we need to start the poll state machine, do it */
-					if (want_start) {
-						start();
-					}
-
-					return OK;
-				}
-			}
-		}
-
-	default:
-		/* give it to the superclass */
-		return I2C::ioctl(filp, cmd, arg);
-	}
-}
-
-ssize_t
-SF1XX::read(device::file_t *filp, char *buffer, size_t buflen)
-{
-	unsigned count = buflen / sizeof(struct distance_sensor_s);
-	struct distance_sensor_s *rbuf = reinterpret_cast<struct distance_sensor_s *>(buffer);
-	int ret = 0;
-
-	/* buffer must be large enough */
-	if (count < 1) {
-		return -ENOSPC;
-	}
-
-	/* if automatic measurement is enabled */
-	if (_measure_interval > 0) {
-
-		/*
-		 * While there is space in the caller's buffer, and reports, copy them.
-		 * Note that we may be pre-empted by the workq thread while we are doing this;
-		 * we are careful to avoid racing with them.
-		 */
-		while (count--) {
-			if (_reports->get(rbuf)) {
-				ret += sizeof(*rbuf);
-				rbuf++;
-			}
-		}
-
-		/* if there was no data, warn the caller */
-		return ret ? ret : -EAGAIN;
-	}
-
-	/* manual measurement - run one conversion */
-	do {
-		_reports->flush();
-
-		/* trigger a measurement */
-		if (OK != measure()) {
-			ret = -EIO;
-			break;
-		}
-
-		/* wait for it to complete */
-		px4_usleep(_conversion_interval);
-
-		/* run the collection phase */
-		if (OK != collect()) {
-			ret = -EIO;
-			break;
-		}
-
-		/* state machine will have generated a report, copy it out */
-		if (_reports->get(rbuf)) {
-			ret = sizeof(*rbuf);
-		}
-
-	} while (0);
-
-	return ret;
-}
-
 int
 SF1XX::measure()
 {
-	int ret;
-
-	/*
-	 * Send the command '0' -- read altitude
-	 */
-
+	// Send the command '0' -- read altitude
 	uint8_t cmd = 0;
-	ret = transfer(&cmd, 1, nullptr, 0);
+	int ret = transfer(&cmd, 1, nullptr, 0);
 
 	if (OK != ret) {
 		perf_count(_comms_errors);
@@ -468,13 +249,13 @@ SF1XX::measure()
 int
 SF1XX::collect()
 {
-	int	ret = -EIO;
-
-	/* read from the sensor */
-	uint8_t val[2] = {0, 0};
 	perf_begin(_sample_perf);
 
-	ret = transfer(nullptr, 0, &val[0], 2);
+	// read from the sensor
+	uint8_t val[2]{};
+
+	const hrt_abstime timestamp_sample = hrt_absolute_time();
+	int ret = transfer(nullptr, 0, &val[0], 2);
 
 	if (ret < 0) {
 		PX4_DEBUG("error reading from sensor: %d", ret);
@@ -486,40 +267,16 @@ SF1XX::collect()
 	uint16_t distance_cm = val[0] << 8 | val[1];
 	float distance_m = float(distance_cm) * 1e-2f;
 
-	struct distance_sensor_s report;
-	report.timestamp = hrt_absolute_time();
-	report.type = distance_sensor_s::MAV_DISTANCE_SENSOR_LASER;
-	report.orientation = _rotation;
-	report.current_distance = distance_m;
-	report.min_distance = get_minimum_distance();
-	report.max_distance = get_maximum_distance();
-	report.variance = 0.0f;
-	report.signal_quality = -1;
-	/* TODO: set proper ID */
-	report.id = 0;
-
-	/* publish it, if we are the primary */
-	if (_distance_sensor_topic != nullptr) {
-		orb_publish(ORB_ID(distance_sensor), _distance_sensor_topic, &report);
-	}
-
-	_reports->force(&report);
-
-	/* notify anyone waiting for data */
-	poll_notify(POLLIN);
-
-	ret = OK;
+	_px4_rangefinder.update(timestamp_sample, distance_m, -1);
 
 	perf_end(_sample_perf);
+
 	return ret;
 }
 
 void
 SF1XX::start()
 {
-	/* reset the report ring and state machine */
-	_reports->flush();
-
 	/* set register to '0' */
 	measure();
 
@@ -553,8 +310,8 @@ SF1XX::print_info()
 {
 	perf_print_counter(_sample_perf);
 	perf_print_counter(_comms_errors);
-	printf("poll interval:  %u\n", _measure_interval);
-	_reports->print_info("report queue");
+
+	_px4_rangefinder.print_status();
 }
 
 /**
@@ -563,14 +320,7 @@ SF1XX::print_info()
 namespace sf1xx
 {
 
-SF1XX	*g_dev;
-
-int 	start(uint8_t rotation);
-int 	start_bus(uint8_t rotation, int i2c_bus);
-int 	stop();
-int 	test();
-int 	reset();
-int 	info();
+SF1XX	*g_dev{nullptr};
 
 /**
  *
@@ -625,19 +375,6 @@ start_bus(uint8_t rotation, int i2c_bus)
 		goto fail;
 	}
 
-	/* set the poll rate to default, starts automatic data collection */
-	fd = px4_open(SF1XX_DEVICE_PATH, O_RDONLY);
-
-	if (fd < 0) {
-		goto fail;
-	}
-
-	if (ioctl(fd, SENSORIOCSPOLLRATE, SENSOR_POLLRATE_DEFAULT) < 0) {
-		px4_close(fd);
-		goto fail;
-	}
-
-	px4_close(fd);
 	return PX4_OK;
 
 fail:
@@ -669,102 +406,11 @@ stop()
 }
 
 /**
- * Perform some basic functional tests on the driver;
- * make sure we can collect data from the sensor in polled
- * and automatic modes.
- */
-int
-test()
-{
-	struct distance_sensor_s report;
-	ssize_t sz;
-	int ret;
-
-	int fd = px4_open(SF1XX_DEVICE_PATH, O_RDONLY);
-
-	if (fd < 0) {
-		PX4_ERR("%s open failed (try 'sf1xx start' if the driver is not running)", SF1XX_DEVICE_PATH);
-		return PX4_ERROR;
-	}
-
-	/* do a simple demand read */
-	sz = read(fd, &report, sizeof(report));
-
-	if (sz != sizeof(report)) {
-		PX4_ERR("immediate read failed");
-		return PX4_ERROR;
-	}
-
-	print_message(report);
-
-	/* start the sensor polling at 2Hz */
-	if (OK != ioctl(fd, SENSORIOCSPOLLRATE, 2)) {
-		PX4_ERR("failed to set 2Hz poll rate");
-		return PX4_ERROR;
-	}
-
-	/* read the sensor 5x and report each value */
-	for (unsigned i = 0; i < 5; i++) {
-		struct pollfd fds;
-
-		/* wait for data to be ready */
-		fds.fd = fd;
-		fds.events = POLLIN;
-		ret = poll(&fds, 1, 2000);
-
-		if (ret != 1) {
-			PX4_ERR("timed out waiting for sensor data");
-			return PX4_ERROR;
-		}
-
-		/* now go get it */
-		sz = read(fd, &report, sizeof(report));
-
-		if (sz != sizeof(report)) {
-			PX4_ERR("periodic read failed");
-			return PX4_ERROR;
-		}
-
-		print_message(report);
-	}
-
-	/* reset the sensor polling to default rate */
-	if (OK != ioctl(fd, SENSORIOCSPOLLRATE, SENSOR_POLLRATE_DEFAULT)) {
-		PX4_ERR("failed to set default poll rate");
-		return PX4_ERROR;
-	}
-
-	px4_close(fd);
-
-	PX4_INFO("PASS");
-	return PX4_OK;
-}
-
-/**
  * Reset the driver.
  */
 int
 reset()
 {
-	int fd = px4_open(SF1XX_DEVICE_PATH, O_RDONLY);
-
-	if (fd < 0) {
-		PX4_ERR("failed");
-		return PX4_ERROR;
-	}
-
-	if (ioctl(fd, SENSORIOCRESET, 0) < 0) {
-		PX4_ERR("driver reset failed");
-		return PX4_ERROR;
-	}
-
-	if (ioctl(fd, SENSORIOCSPOLLRATE, SENSOR_POLLRATE_DEFAULT) < 0) {
-		PX4_ERR("driver poll restart failed");
-		return PX4_ERROR;
-	}
-
-	px4_close(fd);
-
 	return PX4_OK;
 }
 
