CharlesXu1124/Firmware

sensors: move baro aggregation to new sensors/vehicle_air_data

src/modules/sensors/vehicle_air_data/VehicleAirData.cpp
@@ -1,0 +1,297 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2020 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#include "VehicleAirData.hpp"
+
+#include <px4_platform_common/log.h>
+#include <lib/ecl/geo/geo.h>
+
+using namespace matrix;
+using namespace time_literals;
+using math::radians;
+
+static constexpr uint32_t SENSOR_TIMEOUT{300_ms};
+
+VehicleAirData::VehicleAirData() :
+	ModuleParams(nullptr),
+	ScheduledWorkItem(MODULE_NAME, px4::wq_configurations::att_pos_ctrl)
+{
+	_voter.set_timeout(SENSOR_TIMEOUT);
+}
+
+VehicleAirData::~VehicleAirData()
+{
+	Stop();
+
+	perf_free(_cycle_perf);
+}
+
+bool VehicleAirData::Start()
+{
+	ScheduleNow();
+
+	return true;
+}
+
+void VehicleAirData::Stop()
+{
+	Deinit();
+
+	// clear all registered callbacks
+	for (auto &sub : _sensor_sub) {
+		sub.unregisterCallback();
+	}
+}
+
+void VehicleAirData::SensorCorrectionsUpdate(bool force)
+{
+	// check if the selected sensor has updated
+	if (_sensor_correction_sub.updated() || force) {
+
+		sensor_correction_s corrections{};
+
+		if (_sensor_correction_sub.copy(&corrections)) {
+
+			// if selected sensor has changed, find updated index
+			if ((_corrections_selected_instance < 0) || force) {
+				_corrections_selected_instance = -1;
+
+				// find sensor_corrections index
+				for (int i = 0; i < MAX_SENSOR_COUNT; i++) {
+					if (corrections.baro_device_ids[i] == _selected_sensor_device_id) {
+						_corrections_selected_instance = i;
+					}
+				}
+			}
+
+			switch (_corrections_selected_instance) {
+			case 0:
+				_primary_offset = corrections.baro_offset_0;
+				_primary_scale = corrections.baro_scale_0;
+				break;
+
+			case 1:
+				_primary_offset = corrections.baro_offset_1;
+				_primary_scale = corrections.baro_scale_1;
+				break;
+
+			case 2:
+				_primary_offset = corrections.baro_offset_2;
+				_primary_scale = corrections.baro_scale_2;
+				break;
+
+			default:
+				_primary_offset = 0.f;
+				_primary_scale = 1.f;
+			}
+		}
+	}
+}
+
+void VehicleAirData::ParametersUpdate(bool force)
+{
+	// Check if parameters have changed
+	if (_params_sub.updated() || force) {
+		// clear update
+		parameter_update_s param_update;
+		_params_sub.copy(&param_update);
+
+		updateParams();
+	}
+}
+
+void VehicleAirData::Run()
+{
+	perf_begin(_cycle_perf);
+	// TODO:
+	// 1. grab all sensor data and push into voter
+	// 2. check failover
+	//     - register callback to new primary sensor
+	// 3. take current primary data and publish vehicle_air_data
+	//
+	// 5. Schedule timeout callback ()
+
+
+	// Questions??
+	//  - how to handle priorities
+	//  - report failers?
+	//  - structure for setting QNH from GPS
+
+	bool baro_updated[MAX_SENSOR_COUNT] {};
+
+	for (int uorb_index = 0; uorb_index < MAX_SENSOR_COUNT; uorb_index++) {
+
+		if (!_sensor_baro_advertised[uorb_index] && _sensor_sub[uorb_index].advertised()) {
+			_sensor_baro_advertised[uorb_index] = true;
+
+			if (uorb_index > 0) {
+				/* the first always exists, but for each further sensor, add a new validator */
+				if (!_voter.add_new_validator()) {
+					PX4_ERR("failed to add validator for sensor_baro:%i", uorb_index);
+				}
+			}
+		}
+
+		baro_updated[uorb_index] = _sensor_sub[uorb_index].updated();
+
+		if (baro_updated[uorb_index]) {
+			if (_sensor_sub[uorb_index].copy(&_sensor_baro_last[uorb_index])) {
+
+				if (_priority[uorb_index] == 0) {
+					// set initial priority
+					_priority[uorb_index] = _sensor_sub[uorb_index].get_priority();
+				}
+
+				float vect[3] = {_sensor_baro_last[uorb_index].pressure, _sensor_baro_last[uorb_index].temperature, 0.f};
+				_voter.put(uorb_index, _sensor_baro_last[uorb_index].timestamp, vect, _sensor_baro_last[uorb_index].error_count,
+					   _priority[uorb_index]);
+			}
+		}
+	}
+
+	int best_index = -1;
+	_voter.get_best(hrt_absolute_time(), &best_index);
+
+	if (best_index >= 0) {
+		if (_selected_sensor_sub_index != best_index) {
+
+			// clear all registered callbacks
+			for (auto &sub : _sensor_sub) {
+				sub.unregisterCallback();
+			}
+
+			_selected_sensor_sub_index = best_index;
+			_selected_sensor_device_id = _sensor_baro_last[best_index].device_id;
+
+			// reset primary offset and scale
+			_primary_offset = 0.f;
+			_primary_scale = 1.f;
+			_corrections_selected_instance = -1;
+
+			SensorCorrectionsUpdate(true);
+
+			_sensor_sub[_selected_sensor_sub_index].registerCallback();
+		}
+
+	} else {
+		_selected_sensor_sub_index = -1;
+	}
+
+	if ((_selected_sensor_sub_index >= 0) && baro_updated[_selected_sensor_sub_index]) {
+
+		ParametersUpdate();
+		SensorCorrectionsUpdate();
+
+		const sensor_baro_s &baro = _sensor_baro_last[_selected_sensor_sub_index];
+
+		// populate vehicle_air_data with primary baro and publish
+		vehicle_air_data_s out{};
+		out.timestamp_sample = baro.timestamp; // TODO: baro.timestamp_sample;
+		out.baro_device_id = baro.device_id;
+		out.baro_temp_celcius = baro.temperature;
+
+		// Convert from millibar to Pa and apply temperature compensation
+		out.baro_pressure_pa = (100.0f * baro.pressure - _primary_offset) * _primary_scale;
+
+		// calculate altitude using the hypsometric equation
+		static constexpr float T1 = 15.0f - CONSTANTS_ABSOLUTE_NULL_CELSIUS; // temperature at base height in Kelvin
+		static constexpr float a = -6.5f / 1000.0f; // temperature gradient in degrees per metre
+
+		// current pressure at MSL in kPa (QNH in hPa)
+		const float p1 = _param_sens_baro_qnh.get() * 0.1f;
+
+		// measured pressure in kPa
+		const float p = out.baro_pressure_pa * 0.001f;
+
+		/*
+		 * Solve:
+		 *
+		 *     /        -(aR / g)     \
+		 *    | (p / p1)          . T1 | - T1
+		 *     \                      /
+		 * h = -------------------------------  + h1
+		 *                   a
+		 */
+		out.baro_alt_meter = (((powf((p / p1), (-(a * CONSTANTS_AIR_GAS_CONST) / CONSTANTS_ONE_G))) * T1) - T1) / a;
+
+		// calculate air density
+		// estimate air density assuming typical 20degC ambient temperature
+		// TODO: use air temperature if available (differential pressure sensors)
+		static constexpr float pressure_to_density = 1.0f / (CONSTANTS_AIR_GAS_CONST * (20.0f -
+				CONSTANTS_ABSOLUTE_NULL_CELSIUS));
+
+		out.rho = pressure_to_density * out.baro_pressure_pa;
+
+		out.timestamp = hrt_absolute_time();
+		_vehicle_air_data_pub.publish(out);
+	}
+
+	// check failover and report
+	if (_last_failover_count != _voter.failover_count()) {
+		uint32_t flags = _voter.failover_state();
+		int failover_index = _voter.failover_index();
+
+		if (flags == DataValidator::ERROR_FLAG_NO_ERROR) {
+			if (failover_index != -1) {
+				// we switched due to a non-critical reason. No need to panic.
+				PX4_INFO("sensor_baro switch from #%i", failover_index);
+			}
+
+		} else {
+			if (failover_index != -1) {
+				mavlink_log_emergency(&_mavlink_log_pub, "sensor_baro:#%i failed: %s%s%s%s%s!, reconfiguring priorities",
+						      failover_index,
+						      ((flags & DataValidator::ERROR_FLAG_NO_DATA) ? " OFF" : ""),
+						      ((flags & DataValidator::ERROR_FLAG_STALE_DATA) ? " STALE" : ""),
+						      ((flags & DataValidator::ERROR_FLAG_TIMEOUT) ? " TIMEOUT" : ""),
+						      ((flags & DataValidator::ERROR_FLAG_HIGH_ERRCOUNT) ? " ERR CNT" : ""),
+						      ((flags & DataValidator::ERROR_FLAG_HIGH_ERRDENSITY) ? " ERR DNST" : ""));
+
+				// reduce priority of failed sensor to the minimum
+				_priority[failover_index] = ORB_PRIO_MIN;
+			}
+		}
+	}
+
+	// reschedule timeout
+	ScheduleDelayed(SENSOR_TIMEOUT / 2);
+
+	perf_end(_cycle_perf);
+}
+
+void VehicleAirData::PrintStatus()
+{
+	PX4_INFO("selected barometer: %d (%d)", _selected_sensor_device_id, _selected_sensor_sub_index);
+	perf_print_counter(_cycle_perf);
+	_voter.print();
+}

src/modules/sensors/vehicle_air_data/VehicleAirData.hpp
@@ -1,0 +1,106 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2020 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#pragma once
+
+#include <lib/ecl/validation/data_validator_group.h>
+#include <lib/mathlib/math/Limits.hpp>
+#include <lib/matrix/matrix/math.hpp>
+#include <lib/perf/perf_counter.h>
+#include <lib/systemlib/mavlink_log.h>
+#include <px4_platform_common/log.h>
+#include <px4_platform_common/module_params.h>
+#include <px4_platform_common/px4_config.h>
+#include <px4_platform_common/px4_work_queue/ScheduledWorkItem.hpp>
+#include <uORB/Publication.hpp>
+#include <uORB/Subscription.hpp>
+#include <uORB/SubscriptionCallback.hpp>
+#include <uORB/topics/parameter_update.h>
+#include <uORB/topics/sensor_baro.h>
+#include <uORB/topics/sensor_correction.h>
+#include <uORB/topics/vehicle_air_data.h>
+
+class VehicleAirData : public ModuleParams, public px4::ScheduledWorkItem
+{
+public:
+
+	VehicleAirData();
+	~VehicleAirData() override;
+
+	bool Start();
+	void Stop();
+
+	void PrintStatus();
+
+private:
+	void Run() override;
+
+	void ParametersUpdate(bool force = false);
+	void SensorCorrectionsUpdate(bool force = false);
+
+	static constexpr int MAX_SENSOR_COUNT = 3;
+
+	DEFINE_PARAMETERS(
+		(ParamFloat<px4::params::SENS_BARO_QNH>) _param_sens_baro_qnh
+	)
+
+	uORB::Publication<vehicle_air_data_s> _vehicle_air_data_pub{ORB_ID(vehicle_air_data)};
+
+	uORB::Subscription _params_sub{ORB_ID(parameter_update)};
+	uORB::Subscription _sensor_correction_sub{ORB_ID(sensor_correction)};
+
+	uORB::SubscriptionCallbackWorkItem _sensor_sub[MAX_SENSOR_COUNT] {
+		{this, ORB_ID(sensor_baro), 0},
+		{this, ORB_ID(sensor_baro), 1},
+		{this, ORB_ID(sensor_baro), 2}
+	};
+
+	perf_counter_t _cycle_perf{perf_alloc(PC_ELAPSED, MODULE_NAME": cycle")};
+
+	orb_advert_t _mavlink_log_pub{nullptr};
+
+	sensor_baro_s _sensor_baro_last[MAX_SENSOR_COUNT] {};
+	bool _sensor_baro_advertised[MAX_SENSOR_COUNT] {};
+
+	float _primary_offset{0.f};
+	float _primary_scale{1.f};
+
+	uint32_t _selected_sensor_device_id{0};
+	int8_t _selected_sensor_sub_index{-1};
+	int8_t _corrections_selected_instance{-1};
+
+	uint8_t _priority[MAX_SENSOR_COUNT] {}; /**< sensor priority */
+	DataValidatorGroup _voter{1};
+	unsigned _last_failover_count{0};
+
+};

dagar/PX4-Firmware

src/modules/sensors/vehicle_air_data/VehicleAirData.cpp
@@ -1,0 +1,284 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2020 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#include "VehicleAirData.hpp"
+
+#include <px4_platform_common/log.h>
+#include <lib/ecl/geo/geo.h>
+
+using namespace matrix;
+using namespace time_literals;
+using math::radians;
+
+static constexpr uint32_t SENSOR_TIMEOUT{300_ms};
+
+VehicleAirData::VehicleAirData() :
+	ModuleParams(nullptr),
+	ScheduledWorkItem(MODULE_NAME, px4::wq_configurations::att_pos_ctrl)
+{
+	_voter.set_timeout(SENSOR_TIMEOUT);
+}
+
+VehicleAirData::~VehicleAirData()
+{
+	Stop();
+
+	perf_free(_cycle_perf);
+}
+
+bool VehicleAirData::Start()
+{
+	ScheduleNow();
+
+	return true;
+}
+
+void VehicleAirData::Stop()
+{
+	Deinit();
+
+	// clear all registered callbacks
+	for (auto &sub : _sensor_sub) {
+		sub.unregisterCallback();
+	}
+}
+
+void VehicleAirData::SensorCorrectionsUpdate(bool force)
+{
+	// check if the selected sensor has updated
+	if (_sensor_correction_sub.updated() || force) {
+
+		sensor_correction_s corrections{};
+
+		if (_sensor_correction_sub.copy(&corrections)) {
+
+			// if selected sensor has changed, find updated index
+			if ((_corrections_selected_instance < 0) || force) {
+				_corrections_selected_instance = -1;
+
+				// find sensor_corrections index
+				for (int i = 0; i < MAX_SENSOR_COUNT; i++) {
+					if (corrections.baro_device_ids[i] == _selected_sensor_device_id) {
+						_corrections_selected_instance = i;
+					}
+				}
+			}
+
+			switch (_corrections_selected_instance) {
+			case 0:
+				_primary_offset = corrections.baro_offset_0;
+				_primary_scale = corrections.baro_scale_0;
+				break;
+
+			case 1:
+				_primary_offset = corrections.baro_offset_1;
+				_primary_scale = corrections.baro_scale_1;
+				break;
+
+			case 2:
+				_primary_offset = corrections.baro_offset_2;
+				_primary_scale = corrections.baro_scale_2;
+				break;
+
+			default:
+				_primary_offset = 0.f;
+				_primary_scale = 1.f;
+			}
+		}
+	}
+}
+
+void VehicleAirData::ParametersUpdate(bool force)
+{
+	// Check if parameters have changed
+	if (_params_sub.updated() || force) {
+		// clear update
+		parameter_update_s param_update;
+		_params_sub.copy(&param_update);
+
+		updateParams();
+	}
+}
+
+void VehicleAirData::Run()
+{
+	perf_begin(_cycle_perf);
+
+	bool updated[MAX_SENSOR_COUNT] {};
+
+	for (int uorb_index = 0; uorb_index < MAX_SENSOR_COUNT; uorb_index++) {
+
+		if (!_advertised[uorb_index] && _sensor_sub[uorb_index].advertised()) {
+			_advertised[uorb_index] = true;
+
+			if (uorb_index > 0) {
+				/* the first always exists, but for each further sensor, add a new validator */
+				if (!_voter.add_new_validator()) {
+					PX4_ERR("failed to add validator for sensor_baro:%i", uorb_index);
+				}
+			}
+		}
+
+		updated[uorb_index] = _sensor_sub[uorb_index].updated();
+
+		if (updated[uorb_index]) {
+			if (_sensor_sub[uorb_index].copy(&_last_data[uorb_index])) {
+
+				if (_priority[uorb_index] == 0) {
+					// set initial priority
+					_priority[uorb_index] = _sensor_sub[uorb_index].get_priority();
+				}
+
+				float vect[3] = {_last_data[uorb_index].pressure, _last_data[uorb_index].temperature, 0.f};
+				_voter.put(uorb_index, _last_data[uorb_index].timestamp, vect, _last_data[uorb_index].error_count,
+					   _priority[uorb_index]);
+			}
+		}
+	}
+
+	int best_index = -1;
+	_voter.get_best(hrt_absolute_time(), &best_index);
+
+	if (best_index >= 0) {
+		if (_selected_sensor_sub_index != best_index) {
+
+			// clear all registered callbacks
+			for (auto &sub : _sensor_sub) {
+				sub.unregisterCallback();
+			}
+
+			_selected_sensor_sub_index = best_index;
+			_selected_sensor_device_id = _last_data[best_index].device_id;
+
+			// reset primary offset and scale
+			_primary_offset = 0.f;
+			_primary_scale = 1.f;
+			_corrections_selected_instance = -1;
+
+			SensorCorrectionsUpdate(true);
+
+			_sensor_sub[_selected_sensor_sub_index].registerCallback();
+		}
+
+	} else {
+		_selected_sensor_sub_index = -1;
+	}
+
+	if ((_selected_sensor_sub_index >= 0) && updated[_selected_sensor_sub_index]) {
+
+		ParametersUpdate();
+		SensorCorrectionsUpdate();
+
+		const sensor_baro_s &baro = _last_data[_selected_sensor_sub_index];
+
+		// populate vehicle_air_data with primary baro and publish
+		vehicle_air_data_s out{};
+		out.timestamp_sample = baro.timestamp; // TODO: baro.timestamp_sample;
+		out.baro_device_id = baro.device_id;
+		out.baro_temp_celcius = baro.temperature;
+
+		// Convert from millibar to Pa and apply temperature compensation
+		out.baro_pressure_pa = (100.0f * baro.pressure - _primary_offset) * _primary_scale;
+
+		// calculate altitude using the hypsometric equation
+		static constexpr float T1 = 15.0f - CONSTANTS_ABSOLUTE_NULL_CELSIUS; // temperature at base height in Kelvin
+		static constexpr float a = -6.5f / 1000.0f; // temperature gradient in degrees per metre
+
+		// current pressure at MSL in kPa (QNH in hPa)
+		const float p1 = _param_sens_baro_qnh.get() * 0.1f;
+
+		// measured pressure in kPa
+		const float p = out.baro_pressure_pa * 0.001f;
+
+		/*
+		 * Solve:
+		 *
+		 *     /        -(aR / g)     \
+		 *    | (p / p1)          . T1 | - T1
+		 *     \                      /
+		 * h = -------------------------------  + h1
+		 *                   a
+		 */
+		out.baro_alt_meter = (((powf((p / p1), (-(a * CONSTANTS_AIR_GAS_CONST) / CONSTANTS_ONE_G))) * T1) - T1) / a;
+
+		// calculate air density
+		// estimate air density assuming typical 20degC ambient temperature
+		// TODO: use air temperature if available (differential pressure sensors)
+		static constexpr float pressure_to_density = 1.0f / (CONSTANTS_AIR_GAS_CONST * (20.0f -
+				CONSTANTS_ABSOLUTE_NULL_CELSIUS));
+
+		out.rho = pressure_to_density * out.baro_pressure_pa;
+
+		out.timestamp = hrt_absolute_time();
+		_vehicle_air_data_pub.publish(out);
+	}
+
+	// check failover and report
+	if (_last_failover_count != _voter.failover_count()) {
+		uint32_t flags = _voter.failover_state();
+		int failover_index = _voter.failover_index();
+
+		if (flags == DataValidator::ERROR_FLAG_NO_ERROR) {
+			if (failover_index != -1) {
+				// we switched due to a non-critical reason. No need to panic.
+				PX4_INFO("sensor_baro switch from #%i", failover_index);
+			}
+
+		} else {
+			if (failover_index != -1) {
+				mavlink_log_emergency(&_mavlink_log_pub, "sensor_baro:#%i failed: %s%s%s%s%s!, reconfiguring priorities",
+						      failover_index,
+						      ((flags & DataValidator::ERROR_FLAG_NO_DATA) ? " OFF" : ""),
+						      ((flags & DataValidator::ERROR_FLAG_STALE_DATA) ? " STALE" : ""),
+						      ((flags & DataValidator::ERROR_FLAG_TIMEOUT) ? " TIMEOUT" : ""),
+						      ((flags & DataValidator::ERROR_FLAG_HIGH_ERRCOUNT) ? " ERR CNT" : ""),
+						      ((flags & DataValidator::ERROR_FLAG_HIGH_ERRDENSITY) ? " ERR DNST" : ""));
+
+				// reduce priority of failed sensor to the minimum
+				_priority[failover_index] = ORB_PRIO_MIN;
+			}
+		}
+	}
+
+	// reschedule timeout
+	ScheduleDelayed(100_ms);
+
+	perf_end(_cycle_perf);
+}
+
+void VehicleAirData::PrintStatus()
+{
+	PX4_INFO("selected barometer: %d (%d)", _selected_sensor_device_id, _selected_sensor_sub_index);
+	perf_print_counter(_cycle_perf);
+	_voter.print();
+}

src/modules/sensors/vehicle_air_data/VehicleAirData.hpp
@@ -1,0 +1,106 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2020 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#pragma once
+
+#include <lib/ecl/validation/data_validator_group.h>
+#include <lib/mathlib/math/Limits.hpp>
+#include <lib/matrix/matrix/math.hpp>
+#include <lib/perf/perf_counter.h>
+#include <lib/systemlib/mavlink_log.h>
+#include <px4_platform_common/log.h>
+#include <px4_platform_common/module_params.h>
+#include <px4_platform_common/px4_config.h>
+#include <px4_platform_common/px4_work_queue/ScheduledWorkItem.hpp>
+#include <uORB/Publication.hpp>
+#include <uORB/Subscription.hpp>
+#include <uORB/SubscriptionCallback.hpp>
+#include <uORB/topics/parameter_update.h>
+#include <uORB/topics/sensor_baro.h>
+#include <uORB/topics/sensor_correction.h>
+#include <uORB/topics/vehicle_air_data.h>
+
+class VehicleAirData : public ModuleParams, public px4::ScheduledWorkItem
+{
+public:
+
+	VehicleAirData();
+	~VehicleAirData() override;
+
+	bool Start();
+	void Stop();
+
+	void PrintStatus();
+
+private:
+	void Run() override;
+
+	void ParametersUpdate(bool force = false);
+	void SensorCorrectionsUpdate(bool force = false);
+
+	static constexpr int MAX_SENSOR_COUNT = 3;
+
+	DEFINE_PARAMETERS(
+		(ParamFloat<px4::params::SENS_BARO_QNH>) _param_sens_baro_qnh
+	)
+
+	uORB::Publication<vehicle_air_data_s> _vehicle_air_data_pub{ORB_ID(vehicle_air_data)};
+
+	uORB::Subscription _params_sub{ORB_ID(parameter_update)};
+	uORB::Subscription _sensor_correction_sub{ORB_ID(sensor_correction)};
+
+	uORB::SubscriptionCallbackWorkItem _sensor_sub[MAX_SENSOR_COUNT] {
+		{this, ORB_ID(sensor_baro), 0},
+		{this, ORB_ID(sensor_baro), 1},
+		{this, ORB_ID(sensor_baro), 2}
+	};
+
+	perf_counter_t _cycle_perf{perf_alloc(PC_ELAPSED, MODULE_NAME": cycle")};
+
+	orb_advert_t _mavlink_log_pub{nullptr};
+
+	sensor_baro_s _last_data[MAX_SENSOR_COUNT] {};
+	bool _advertised[MAX_SENSOR_COUNT] {};
+
+	float _primary_offset{0.f};
+	float _primary_scale{1.f};
+
+	uint32_t _selected_sensor_device_id{0};
+	int8_t _selected_sensor_sub_index{-1};
+	int8_t _corrections_selected_instance{-1};
+
+	uint8_t _priority[MAX_SENSOR_COUNT] {}; /**< sensor priority */
+	DataValidatorGroup _voter{1};
+	unsigned _last_failover_count{0};
+
+};