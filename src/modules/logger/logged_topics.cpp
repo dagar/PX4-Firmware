kola1197/Firmware

move GPS aggregation and blending to sensors.
Co-authored-by: Daniel Agar <daniel@agar.ca>

src/modules/logger/logged_topics.cpp
@@ -158,7 +158,7 @@ void LoggedTopics::add_estimator_replay_topics()
 {
 	// for estimator replay (need to be at full rate)
 	add_topic("ekf2_timestamps");
-	add_topic("ekf_gps_position");
+	add_topic("sensor_gps");
 
 	// current EKF2 subscriptions
 	add_topic("airspeed");

dagar/PX4-Firmware

src/modules/logger/logged_topics.cpp
@@ -158,7 +158,7 @@ void LoggedTopics::add_estimator_replay_topics()
 {
 	// for estimator replay (need to be at full rate)
 	add_topic("ekf2_timestamps");
-	add_topic("ekf_gps_position");
+	add_topic_multi("sensor_gps");
 
 	// current EKF2 subscriptions
 	add_topic("airspeed");