859eaa5f222380e6026d86b88adb11e30d423f3a
Daniel Agar <daniel@agar.ca>
2021-11-17 16:34:55 -0500

    ekf2: expand gyro bias stability criteria

--- src/modules/ekf2/EKF2.cpp
+++ src/modules/ekf2/EKF2.cpp
@@ -1836,17 +1836,33 @@ void EKF2::UpdateAccelCalibration(const hrt_abstime &timestamp)
 
 void EKF2::UpdateGyroCalibration(const hrt_abstime &timestamp)
 {
+	// State variance assumed for accelerometer bias storage.
+	// This is a reference variance used to calculate the fraction of learned accelerometer bias that will be used to update the stored value.
+	// Larger values cause a larger fraction of the learned biases to be used.
+	static constexpr float max_var_allowed = 1e-3f;
+	static constexpr float min_var_allowed = max_var_allowed * 1e-2f;
+
+	const Vector3f bias_variance{_ekf.getGyroBiasVariance()};
+
 	// Check if conditions are OK for learning of gyro bias values
 	// the EKF is operating in the correct mode and there are no filter faults
-	if (_ekf.control_status_flags().in_air && (_ekf.fault_status().value == 0)) {
+	if ((_ekf.fault_status().value == 0)
+	    && !_ekf.control_status_flags().vehicle_at_rest
+	    && (bias_variance.max() < max_var_allowed)
+	    && (bias_variance.min() > min_var_allowed)
+	   ) {
 
 		if (_gyro_cal.last_us != 0) {
 			_gyro_cal.total_time_us += timestamp - _gyro_cal.last_us;
 
 			// Start checking gyro bias estimates when we have accumulated sufficient calibration time
 			if (_gyro_cal.total_time_us > 30_s) {
+				if (!_gyro_cal.cal_available) {
+					PX4_DEBUG("%d gyro bias now stable", _instance);
+				}
+
 				_gyro_cal.last_bias = _ekf.getGyroBias();
-				_gyro_cal.last_bias_variance = _ekf.getGyroBiasVariance();
+				_gyro_cal.last_bias_variance = bias_variance;
 				_gyro_cal.cal_available = true;
 			}
 		}
@@ -1854,15 +1870,14 @@ void EKF2::UpdateGyroCalibration(const hrt_abstime &timestamp)
 		_gyro_cal.last_us = timestamp;
 
 	} else {
-		// conditions are NOT OK for learning gyro bias, reset timestamp
-		// but keep the accumulated calibration time
-		_gyro_cal.last_us = 0;
-
-		if (_ekf.fault_status().value != 0) {
-			// if a filter fault has occurred, assume previous learning was invalid and do not
-			// count it towards total learning time.
-			_gyro_cal.total_time_us = 0;
+		// conditions are NOT OK for learning gyro bias, reset
+		if (_gyro_cal.total_time_us > 0) {
+			PX4_DEBUG("%d, clearing learned gyro bias", _instance);
 		}
+
+		_gyro_cal.last_us = 0;
+		_gyro_cal.total_time_us = 0;
+		_gyro_cal.cal_available = false;
 	}
 }
 
