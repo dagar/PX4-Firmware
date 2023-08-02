af75d2fc370cd84fb39926953d0ad95f4a6a4966
Daniel Agar <daniel@agar.ca>
2023-08-02 18:00:34 -0400

    WIP

---
+++ platforms/ros2/cmake/px4_add_node.cmake
@@ -0,0 +1,11 @@
+
+
+
+
+# find_package(rclcpp REQUIRED)
+# add_executable(minimal_cpp_node src/my_cpp_node.cpp)
+# ament_target_dependencies(minimal_cpp_node rclcpp)
+# install(TARGETS
+#   minimal_cpp_node
+#   DESTINATION lib/${PROJECT_NAME}
+# )
