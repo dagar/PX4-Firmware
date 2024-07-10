ddf8d08202c918f05356b5ff867846b3d95226fc
Daniel Agar <daniel@agar.ca>
2024-07-10 13:25:14 -0400

    ekf2: rng only reset vz as a last resort

--- src/modules/ekf2/EKF/aid_sources/range_finder/range_height_control.cpp
+++ src/modules/ekf2/EKF/aid_sources/range_finder/range_height_control.cpp
@@ -180,8 +180,10 @@ void Ekf::controlRangeHaglFusion()
 					_information_events.flags.reset_hgt_to_rng = true;
 					resetVerticalPositionTo(-(aid_src.observation - _state.terrain));
 
-					// reset vertical velocity
-					resetVerticalVelocityToZero();
+					// reset vertical velocity if no valid sources available
+					if (!isVerticalVelocityAidingActive()) {
+						resetVerticalVelocityToZero();
+					}
 
 					aid_src.time_last_fuse = _time_delayed_us;
 
