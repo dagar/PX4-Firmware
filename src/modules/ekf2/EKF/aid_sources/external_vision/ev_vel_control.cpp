86a8cca291fafa27c7269da705d50406c23b5c53
Daniel Agar <daniel@agar.ca>
2024-07-10 13:20:16 -0400

    ekf2: EV velocity control should own vertical velocity reset if height failing

--- src/modules/ekf2/EKF/aid_sources/external_vision/ev_vel_control.cpp
+++ src/modules/ekf2/EKF/aid_sources/external_vision/ev_vel_control.cpp
@@ -210,6 +210,17 @@ void Ekf::controlEvVelFusion(const extVisionSample &ev_sample, const bool common
 
 					stopEvVelFusion();
 				}
+
+			} else if (isHeightResetRequired()) {
+				// reset vertical velocity if height is failing
+				if (ev_sample.vel_frame == VelocityFrame::BODY_FRAME_FRD) {
+					const Vector3f measurement_ekf_frame = _R_to_earth * measurement;
+					const Vector3f measurement_var_ekf_frame = rotateVarianceToEkf(measurement_var);
+					resetVerticalVelocityTo(measurement_ekf_frame(2), measurement_var_ekf_frame(2));
+
+				} else {
+					resetVelocityTo(measurement, measurement_var);
+				}
 			}
 
 		} else {
